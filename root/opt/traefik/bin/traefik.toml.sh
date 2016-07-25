b6819a5dbdc742a3126b2f6157186ad9879ba2b7
Raul Sanchez <rawmind@gmail.com>
2016-07-25 19:19:04 +0200

    Feat: Added https modes selector

--- root/opt/traefik/bin/traefik.toml.sh
+++ root/opt/traefik/bin/traefik.toml.sh
@@ -1,25 +1,38 @@
 #!/usr/bin/env sh
 
+TRAEFIK_ENTRYPOINTS_HTTP="\
+  [entryPoints.http]
+  address = \":${TRAEFIK_HTTP_PORT}\""
+
 # Check that you provide key/crt files and add them to the config
 filelist=`ls -1 ${TRAEFIK_SSL_PATH}/*.key | cut -d"." -f1`
 RC=`echo $?`
 
 if [ $RC -eq 0 ]; then
-    TRAEFIK_SSL_OPTS="\
+    TRAEFIK_ENTRYPOINTS_HTTPS="\
   [entryPoints.https]
   address = \":${TRAEFIK_HTTPS_PORT}\"
     [entryPoints.https.tls]"
     for i in $filelist; do
         if [ -f "$i.crt" ]; then
-            TRAEFIK_SSL_OPTS=$TRAEFIK_SSL_OPTS"
+            TRAEFIK_ENTRYPOINTS_HTTPS=$TRAEFIK_ENTRYPOINTS_HTTPS"
       [[entryPoints.https.tls.certificates]]
       certFile = \"${i}.crt\"
       keyFile = \"${i}.key\""
         fi
     done
-    TRAEFIK_ENTRYPOINTS='"http", "https"'
 fi
 
+if [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xtrue" ]; then
+    TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}${TRAEFIK_ENTRYPOINTS_HTTPS}
+    TRAEFIK_ENTRYPOINTS='"http", "https"'
+elif [ "X${TRAEFIK_HTTPS_ENABLE}" == "Xonly" ]; then
+    TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTPS}
+    TRAEFIK_ENTRYPOINTS='"https"'
+else 
+    TRAEFIK_ENTRYPOINTS_OPTS=${TRAEFIK_ENTRYPOINTS_HTTP}
+    TRAEFIK_ENTRYPOINTS='"http"'
+fi
 
 cat << EOF > ${SERVICE_HOME}/etc/traefik.toml
 # traefik.toml
@@ -28,9 +41,7 @@ traefikLogsFile = "${TRAEFIK_LOG_FILE}"
 accessLogsFile = "${SERVICE_HOME}/log/access.log"
 defaultEntryPoints = [${TRAEFIK_ENTRYPOINTS}]
 [entryPoints]
-  [entryPoints.http]
-  address = ":${TRAEFIK_HTTP_PORT}"
-  ${TRAEFIK_SSL_OPTS}
+  ${TRAEFIK_ENTRYPOINTS_OPTS}
 [web]
 address = ":${TRAEFIK_ADMIN_PORT}"
 
